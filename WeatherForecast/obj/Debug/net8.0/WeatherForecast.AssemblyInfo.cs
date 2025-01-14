d740818e120623c993e77afb0e7e59f55e0a805f
Lacika16 <klimal@kkszki.hu>
2025-01-14 11:19:40 +0100

    Packages are ready to use

--- WeatherForecast/obj/Debug/net8.0/WeatherForecast.AssemblyInfo.cs
+++ WeatherForecast/obj/Debug/net8.0/WeatherForecast.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherForecast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e4eb2ffcb6c45cdf011dcee27fa0cb9f81ba1a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherForecast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherForecast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
